e05db38f2df55ab5233bfa25e786fb774a3d1a6e
LeeKuanGim <leekuangim@gmail.com>
2021-12-19 03:48:24 +0000

    commit 3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,15 @@
-<sciript>
+<script>
     let user_name="";
     let age="";
 
     async function getAge() {
-  fetch('https://api.agify.io/?name' + user_name)
-	.then((response) => response.json())
-	.then((data) => {
-  	user_name = data.name;
-  	age = data.age;
-	});
-}
+        fetch('https://api.agify.io/?name=' + user_name)
+            .then((response) => response.json())
+            .then((data) => {
+            user_name = data.name;
+            age = data.age;
+            });
+        }
 </script>
 <h1>Guess Age</h1>
 
@@ -18,3 +18,4 @@
     <input id="user_name" bind:value={user_name} type="text" />
     <button type="submit">Check</button>
 </form>
+<p>Age:{age}</p>
